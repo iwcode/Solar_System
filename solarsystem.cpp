cd686042314b6bf25d611699cab0303bcd9c44d1
iwcode <iwcode@wcc.fun>
2018-08-17 20:42:51 +0800

    first update

---
+++ solarsystem.cpp
@@ -0,0 +1,27 @@
+//solarsystem.cpp
+
+#include "solarsystem.hpp"
+
+#define TIMEPAST 1
+#define SELFROTATE 3
+
+enum STARS {Sun, Mercury, Venus, Earth, Moon, Mars, Jupiter, Saturn, Uranus, NEPTUME};
+
+void SolarSystem::onDisplay() {
+	//TODO:
+}
+
+void SolarSystem::onUpdate() {
+	//TODO:
+}
+
+void SolarSystem::onKeyboard(unsigned char key, int x, int y) {
+	//TODO:
+}
+
+SolarSystem::SolarSystem() {
+	//TODO:
+}
+SolarSystem::~SolarSystem() {
+	//TODO:
+}
